f68dde2dc4fbbf3d5da3f12b1400abe2b56d5611
Emmy <emmy@Emmys-MacBook-Pro.local>
2026-07-21 09:06:21 +0100

    Enable S3 remote state for both environments

--- terraform/environments/environment-a/backend.tf
+++ terraform/environments/environment-a/backend.tf
@@ -1,15 +1,15 @@
-# Remote state backend. Commented out so the first apply can run on LOCAL state.
+# Remote state backend. Comment out to run on LOCAL state.
 # To use remote state: apply terraform/bootstrap first, confirm the bucket name below
 # matches its state_bucket_name output, uncomment, then run:
 #   terraform init -migrate-state
-# The bucket name must match bootstrap/terraform.tfvars (change both if you edit it).
+# The bucket name must match bootstrap/terraform.tfvars.
 
-# terraform {
-#   backend "s3" {
-#     bucket         = "zt-dissertation-tfstate-euw2-7q3k9"
-#     key            = "environment-a/terraform.tfstate"
-#     region         = "eu-west-2"
-#     dynamodb_table = "zt-dissertation-tf-locks"
-#     encrypt        = true
-#   }
-# }
+terraform {
+    backend "s3" {
+    bucket         = "zt-dissertation-tfstate-emmanuelal99"
+    key            = "environment-a/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "zt-dissertation-tf-locks"
+    encrypt        = true
+  }
+}
--- terraform/environments/environment-b/backend.tf
+++ terraform/environments/environment-b/backend.tf
@@ -1,13 +1,13 @@
 # Remote state backend. Fill in bucket/table from the bootstrap outputs, then run
-# `terraform init`. Commented out so the environment can be initialised locally first if
+# `terraform init`. Comment out the environment to be initialised locally first if
 # the backend has not been bootstrapped yet.
 
-# terraform {
-#   backend "s3" {
-#     bucket         = "zt-dissertation-tfstate-CHANGEME"
-#     key            = "environment-b/terraform.tfstate"
-#     region         = "eu-west-2"
-#     dynamodb_table = "zt-dissertation-tf-locks"
-#     encrypt        = true
-#   }
-# }
+terraform {
+    backend "s3" {
+    bucket         = "zt-dissertation-tfstate-emmanuelal99"
+    key            = "environment-b/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "zt-dissertation-tf-locks"
+    encrypt        = true
+  }
+}
